46ff20addae49d739faceb3a3616ccc44ef232ae
adam-ubuntu <gy.adam90@gmail.com>
2020-04-06 16:23:53 -0700

    Add test case for homepage

--- backend/test_flaskr.py
+++ backend/test_flaskr.py
@@ -5,6 +5,7 @@ from flask_sqlalchemy import SQLAlchemy
 
 from flaskr import create_app
 from models import setup_db, Question, Category
+from config import usr, pwd
 
 
 class TriviaTestCase(unittest.TestCase):
@@ -15,7 +16,7 @@ class TriviaTestCase(unittest.TestCase):
         self.app = create_app()
         self.client = self.app.test_client
         self.database_name = "trivia_test"
-        self.database_path = "postgres://{}/{}".format('localhost:5432', self.database_name)
+        self.database_path = "postgresql://" + usr + ":" + pwd + "@localhost:5432/" + self.database_name
         setup_db(self.app, self.database_path)
 
         # binds the app to the current context
@@ -33,6 +34,13 @@ class TriviaTestCase(unittest.TestCase):
     TODO
     Write at least one test for each test for successful operation and for expected errors.
     """
+    def test_get_paginated_questions(self):
+        res = self.client().get('/')
+        data = json.loads(res.data)
+
+        self.assertEqual(res.status_code, 200)
+        self.assertTrue(data['total_questions'])
+        self.assertTrue(len(data['questions']))
 
 
 # Make the tests conveniently executable

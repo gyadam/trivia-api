b34bd639f93cbaea947f3139092758b2f9ee5118
adam-ubuntu <gy.adam90@gmail.com>
2020-04-06 17:05:40 -0700

    Add endpoint for homepage

--- backend/flaskr/__init__.py
+++ backend/flaskr/__init__.py
@@ -27,6 +27,20 @@ def create_app(test_config=None):
   for all available categories.
   '''
 
+  @app.route('/')
+  # return paginated questions
+  def get_questions():
+    page = request.args.get('page', 1, type=int)
+    start = (page - 1) * 10
+    end = start + 10
+    questions = Question.query.all()
+    formatted_questions = [question.format() for question in questions]
+    return jsonify({
+      'success': True,
+      'questions': formatted_questions[start:end],
+      'total_questions': len(formatted_questions)
+    })
+
 
   '''
   #TODO: 
